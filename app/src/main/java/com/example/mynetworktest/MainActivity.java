3bf963cb804486a982fb01b43b0631552295c27e
elmedinasaldic <elmedinasa@edu.aau.at>
2019-03-18 19:50:37 +0100

    Verbindung nun stabil Ouersumme wird gebildet inkl. Antwort ob gerade oder ungerade Zahl

--- app/src/main/java/com/example/mynetworktest/MainActivity.java
+++ app/src/main/java/com/example/mynetworktest/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.mynetworktest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
+    private Button buttonAbschicken;
+    private EditText editTextMatrikelnummer;
+    private TextView textVewAntwort;
+    private Client client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAbschicken = (Button)findViewById(R.id.buttonAbschicken);
+        editTextMatrikelnummer   = (EditText)findViewById(R.id.editTextMatrikelnummer);
+        textVewAntwort = (TextView)findViewById(R.id.textViewAntwort);
+    }
+
+    public void onClick(View v) {
+        Log.v("LogMessage", editTextMatrikelnummer.getText().toString());
+
+
+        client = new Client(editTextMatrikelnummer.getText().toString());
+        client.setTextViewAntwort(textVewAntwort);
+        client.execute("Client connecting");
     }
 }
